afb47810cfe675f799c6308f4c8ebb4bf3432018
cgpvdg <79369600+cgpvdg@users.noreply.github.com>
2026-04-05 13:35:25 +0200

    menu inklap

--- JeugdcrossCompetitie/src/App.vue
+++ JeugdcrossCompetitie/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { computed } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { computed, ref, watch } from 'vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import { useCompetitionData } from './composables/useCompetitionData'
 
 const { siteContent } = useCompetitionData()
+const route = useRoute()
+const mobileMenuOpen = ref(false)
 
 const reglementLink = computed(() => {
   const configured = siteContent.value.reglementUrl || '/docs/competitiereglement-v2.pdf'
@@ -18,6 +20,21 @@ const headerLogoUrl = computed(() => {
   const base = import.meta.env.BASE_URL || '/'
   return new URL('images/Logo.png', `http://local${base}`).pathname
 })
+
+function toggleMobileMenu() {
+  mobileMenuOpen.value = !mobileMenuOpen.value
+}
+
+function closeMobileMenu() {
+  mobileMenuOpen.value = false
+}
+
+watch(
+  () => route.fullPath,
+  () => {
+    closeMobileMenu()
+  },
+)
 </script>
 
 <template>
@@ -33,20 +50,29 @@ const headerLogoUrl = computed(() => {
             <span class="brand-title-white">COMPETITIE</span>
           </div>
         </div>
-        <nav class="nav-links">
-          <RouterLink :to="{ path: '/', hash: '#welkom' }">
+        <button
+          type="button"
+          class="menu-toggle"
+          :aria-expanded="mobileMenuOpen ? 'true' : 'false'"
+          aria-controls="main-nav"
+          @click="toggleMobileMenu"
+        >
+          <i class="fa-solid fa-bars" />
+        </button>
+        <nav id="main-nav" class="nav-links" :class="{ 'is-open': mobileMenuOpen }">
+          <RouterLink :to="{ path: '/', hash: '#welkom' }" @click="closeMobileMenu">
             <i class="fa-solid fa-house" /> Welkom
           </RouterLink>
-          <a :href="reglementLink" download="competitiereglement-v2.pdf">
+          <a :href="reglementLink" download="competitiereglement-v2.pdf" @click="closeMobileMenu">
             <i class="fa-regular fa-file-lines" /> Reglement
           </a>
-          <RouterLink to="/wedstrijden">
+          <RouterLink to="/wedstrijden" @click="closeMobileMenu">
             <i class="fa-solid fa-flag-checkered" /> Wedstrijden
           </RouterLink>
-          <RouterLink to="/individueel">
+          <RouterLink to="/individueel" @click="closeMobileMenu">
             <i class="fa-regular fa-user" /> Individueel
           </RouterLink>
-          <RouterLink to="/ploegen">
+          <RouterLink to="/ploegen" @click="closeMobileMenu">
             <i class="fa-solid fa-users" /> Ploegen
           </RouterLink>
         </nav>
