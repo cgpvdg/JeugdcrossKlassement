db76530aea2a86e6f3b1c2ff97b5253972ce3c07
cgpvdg <79369600+cgpvdg@users.noreply.github.com>
2026-04-05 13:50:41 +0200

    routes

--- JeugdcrossCompetitie/vite.config.js
+++ JeugdcrossCompetitie/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JeugdcrossCompetitie/',
+  base: '/JeugdcrossKlassement/Competitie/',
   plugins: [
     vue(),
     vueDevTools(),
--- JeugdcrossKlassement/vite.config.js
+++ JeugdcrossKlassement/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JeugdcrossKlassement/',
+  base: '/JeugdcrossKlassement/Klassement/',
   plugins: [
     vue(),
     vueDevTools(),
